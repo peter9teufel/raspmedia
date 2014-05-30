2fb185db54fcffc23f5bee9d5a154ff645c6f13e
Peter Neunteufel <peter9teufel@gmail.com>
2014-05-30 10:41:41 +0200

    Image optimization done, checked with bigger portion of 50 images, processing and sending working, new problem with getting large filelist from player occured now, needs further checking, cross test on mac now.

--- Desktop/packages/rmutil/ImageUtil.py
+++ Desktop/packages/rmutil/ImageUtil.py
@@ -27,14 +27,17 @@ def _optimizeCrop(fileName, basePath, destPath, maxW, maxH):
         #print "Saving optimized image..."
         #print "W: ", width
         #print "H: ", height
+        print "Imagesize %d x %d" % (width, height)
         if width == 1920:
+            print "Cropping height"
             # crop upper and lower part
             diff = height - 1080
-            img.crop((0,diff/2,width,height-diff/2))
+            img = img.crop((0,diff/2,width,height-diff/2))
         else:
+            print "Cropping width"
             # crop left and right part
             diff = width - 1920
-            img.crop((diff/2,0,width-diff/2,height))
+            img = img.crop((diff/2,0,width-diff/2,height))
         img.save(destFilePath, 'JPEG', quality=90)
 
 def _optimizeFit(fileName, basePath, destPath, maxW, maxH):
@@ -47,6 +50,8 @@ def _optimizeFit(fileName, basePath, destPath, maxW, maxH):
             img.load()
         except IOError:
             print "IOError in loading PIL image while optimizing, filling up with grey pixels..."
+        # check exif data if image was originally rotated
+        img = _checkOrientation(img)
         maxW = 1920
         maxH = 1080
         w,h = img.size
@@ -62,7 +67,24 @@ def _optimizeFit(fileName, basePath, destPath, maxW, maxH):
         #print "H: ", height
         img.save(destFilePath, 'JPEG', quality=90)
 
-def OptimizeImages(files, basePath, destPath, maxW=1920, maxH=1080, isWindows=True, style=OPT_CROP):
+def _checkOrientation(img):
+    try :
+        for orientation in ExifTags.TAGS.keys() : 
+            if ExifTags.TAGS[orientation]=='Orientation' : break 
+        exif=dict(img._getexif().items())
+
+        if   exif[orientation] == 3 : 
+            img=img.rotate(180, expand=True)
+        elif exif[orientation] == 6 : 
+            img=img.rotate(270, expand=True)
+        elif exif[orientation] == 8 : 
+            img=img.rotate(90, expand=True)
+    except:
+        pass
+        # print "Exif data not present or can not be processed, returning unmodified image..."
+    return img
+
+def OptimizeImages(files, basePath, destPath, maxW=1920, maxH=1080, isWindows=True, style=OPT_FIT):
     prgDialog = wx.ProgressDialog("Optimizing images...", "Optimizing images for your player...", maximum=len(files), style=wx.PD_AUTO_HIDE)
     cnt = 0
     for filename in files:

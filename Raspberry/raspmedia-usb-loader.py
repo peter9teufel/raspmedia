45525a5d32b33807198605c1afec934d2cb36269
Peter Neunteufel <peter9teufel@gmail.com>
2014-11-03 12:29:31 +0100

    Typo fix.

--- Raspberry/raspmedia-usb-loader.py
+++ Raspberry/raspmedia-usb-loader.py
@@ -27,19 +27,20 @@ def CopyMediaFiles():
     print "Copying files from USB to RaspMedia Player"
     
     for file in os.listdir(USB_MEDIA_PATH):
-        if file.endswith((SUPPORTED_IMAGE_EXTENSIONS)):
-            OptimizeAndCopyImage(file, USB_MEDIA_PATH, MEDIA_PATH)
-        elif file.endswith((SUPPORTED_VIDEO_EXTENSIONS)):
-            print "Copy video file: ", file
-            size = (os.stat(USB_MEDIA_PATH + '/' + file).st_size)
-            size /= 1024
-            size /= 1024
-            fileSize = str(size) + 'MB'
-            print "Size: ", fileSize
-            print "Please wait..."
-            srcFile = USB_MEDIA_PATH + '/' + file
-            dstFile = MEDIA_PATH + '/' + file
-            shutil.copyfile(srcFile, dstFile)
+        if not file.startswith('.'):
+            if file.endswith((SUPPORTED_IMAGE_EXTENSIONS)):
+                OptimizeAndCopyImage(file, USB_MEDIA_PATH, MEDIA_PATH)
+            elif file.endswith((SUPPORTED_VIDEO_EXTENSIONS)):
+                print "Copy video file: ", file
+                size = (os.stat(USB_MEDIA_PATH + '/' + file).st_size)
+                size /= 1024
+                size /= 1024
+                fileSize = str(size) + 'MB'
+                print "Size: ", fileSize
+                print "Please wait..."
+                srcFile = USB_MEDIA_PATH + '/' + file
+                dstFile = MEDIA_PATH + '/' + file
+                shutil.copyfile(srcFile, dstFile)
 
     print "Media files copied to player successfully!"
 
@@ -47,47 +48,46 @@ def CopyMediaFiles():
 def OptimizeAndCopyImage(fileName, basePath, destPath, maxW=1280, maxH=720, minW=400, minH=400):
     filePath = basePath + '/' + fileName
     destFilePath = destPath + '/' + fileName
-    if filePath.endswith((IMAGE_EXTENSION)):
-        #print "Opening image " + filePath
-        img = Image.open(str(filePath))
-        try:
-            img.load()
-        except IOError:
-            print "IOError in loading PIL image while optimizing, filling up with grey pixels..."
-        # check exif data if image was originally rotated
-        #img = _checkOrientation(img)
-        w,h = img.size
+    #print "Opening image " + filePath
+    img = Image.open(str(filePath))
+    try:
+        img.load()
+    except IOError:
+        print "IOError in loading PIL image while optimizing, filling up with grey pixels..."
+    # check exif data if image was originally rotated
+    #img = _checkOrientation(img)
+    w,h = img.size
 
-        if w > maxW or h > maxH:
-            if w/h > 1.770:
-                width = maxW
-                height = maxW * h / w
-            else:
-                height = maxH
-                width = maxH * w / h
-            if width < w and height < h:
-                img.thumbnail((width,height))
-            else:
-                img = img.resize((width, height))
-        elif w < minW or h < minH:
-            if w/h > 1.770:
-                width = minW
-                height = minW * h / w
-            else:
-                height = minH
-                width = minH * w / h
-            if width < w and height < h:
-                img.thumbnail((width,height))
-            else:
-                img = img.resize((width, height))
+    if w > maxW or h > maxH:
+        if w/h > 1.770:
+            width = maxW
+            height = maxW * h / w
         else:
-            width = w
-            height = h
-        print "Saving optimized image: %d x %d at path %s" % (width,height,destFilePath)
-        if(fileName.endswith('.png') or fileName.endswith('.PNG')):
-            img.save(destFilePath, 'PNG')
+            height = maxH
+            width = maxH * w / h
+        if width < w and height < h:
+            img.thumbnail((width,height))
         else:
-            img.save(destFilePath, 'JPEG', quality=90)
+            img = img.resize((width, height))
+    elif w < minW or h < minH:
+        if w/h > 1.770:
+            width = minW
+            height = minW * h / w
+        else:
+            height = minH
+            width = minH * w / h
+        if width < w and height < h:
+            img.thumbnail((width,height))
+        else:
+            img = img.resize((width, height))
+    else:
+        width = w
+        height = h
+    print "Saving optimized image: %d x %d at path %s" % (width,height,destFilePath)
+    if(fileName.endswith('.png') or fileName.endswith('.PNG')):
+        img.save(destFilePath, 'PNG')
+    else:
+        img.save(destFilePath, 'JPEG', quality=90)
 
 
 # Main Method
